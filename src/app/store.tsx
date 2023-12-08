173f0fba4bfbc4182d80a6b41747a1d2b2d9834c
Matt5246 <116891485+Matt5246@users.noreply.github.com>
2023-12-08 09:34:02 +0100

    LanguagePack in userMenu
    usermenu and redux slice

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -3,10 +3,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './slices/api.slice'
 import authReducer from './slices/auth.slice'
 import usersReducer from './slices/usersSlice'
+import languageSlice from './slices/language.slice'
 
 export default configureStore({
   reducer: {
     users: usersReducer,
+    language: languageSlice,
     auth: authReducer,
     [apiSlice.reducerPath]: apiSlice.reducer
   },
--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,34 +1,49 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react';
 import { Menu, Badge } from 'antd';
 import { UserOutlined, ScheduleOutlined } from '@ant-design/icons';
 import { useSelector } from 'react-redux';
-
-import { User } from '../ts/interfaces';
 import { NavLink } from 'react-router-dom';
+import { User } from '../ts/interfaces';
 
 const UserMenu: React.FC = () => {
-  //@ts-ignore
-  const users: User[] = useSelector(state => state.users.users);
+  const users: User[] = useSelector((state:{users: {users:User[]}}) => state.users.users);
+  const language: string = useSelector((state:{language:{currentLanguage:string }}) => state.language.currentLanguage); 
+  const [languagePack, setLanguagePack] = useState("");
+
+  useEffect(() => {
+    const fetchLanguagePack = async () => {
+      try {
+        let pack = await import(`../assets/translations/${language}.json`); 
+        setLanguagePack(pack);
+      } catch (error) {
+        console.error(`Failed to load language pack for ${language}`, error);
+      }
+    };
+
+    fetchLanguagePack();
+  }, [language]);
 
   return (
     <div>
-      <Menu theme="dark" mode="inline" >
+      <Menu theme="dark" mode="inline">
         <Menu.Item key="-1" icon={<UserOutlined />}>
-          <NavLink to="/home/">People</NavLink>
+          <NavLink to="/home/" >{languagePack.people}</NavLink>
           <Badge count={users.length} style={{ marginLeft: '8px' }} />
         </Menu.Item>
         <Menu.Item key="0" icon={<ScheduleOutlined />}>
-          <NavLink to="/home/Announcements">Announcements</NavLink>
+          <NavLink to="/home/Announcements">{languagePack.announcements}</NavLink>
         </Menu.Item>
-        <span style={{ color: 'gray', marginLeft: '16px', fontSize: 10 }}>Channels</span>
-        {users.map(person => (
+        <span style={{ color: 'gray', marginLeft: '16px', fontSize: 10 }}>
+          {languagePack.channels}
+        </span>
+        {users.map((person) => (
           <Menu.Item key={person.id} icon={<UserOutlined />}>
             <NavLink to={`/home/chat/${person.name}`}>{person.name}</NavLink>
           </Menu.Item>
         ))}
       </Menu>
     </div>
-  )
-}
+  );
+};
 
-export default UserMenu
+export default UserMenu;
