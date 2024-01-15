e9c0d989812d8db7e2fd718f906cfe535184a227
Matt5246 <116891485+Matt5246@users.noreply.github.com>
2024-01-16 00:57:39 +0100

    Init
    UserMenu tour and AnnouncementsTour
    available in settings

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,17 +1,48 @@
 import React from 'react'
 import { Menu, Badge, Avatar } from 'antd';
 import { UserOutlined, ScheduleOutlined, GlobalOutlined } from '@ant-design/icons';
-import { useSelector } from 'react-redux';
 import { User } from '../ts/interfaces';
 import { NavLink } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import Tour from 'antd/lib/tour';
+import type { TourProps } from 'antd';
+import { setOpenModalUserMenu } from '../app/slices/tour.slice';
+import { useSelector, useDispatch } from 'react-redux';
 
 const UserMenu: React.FC = () => {
   //@ts-ignore
   const { favourites } = useSelector((state: any) => state.favourites);
   const language: string = useSelector((state: { language: { currentLanguage: string } }) => state.language.currentLanguage);
   const [languagePack, setLanguagePack] = useState<any>("");
+  const dispatch = useDispatch();
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
+  const ref4 = useRef(null);
 
+  const open = useSelector((state: any) => state.tour.openModalUserMenu)
+  const steps: TourProps['steps'] = [
+    {
+      title: 'People',
+      description: 'here you can Search Quark members and add them to chat.',
+      target: () => ref1.current,
+    },
+    {
+      title: 'Announcements',
+      description: 'There you will find Quark Announcements meetings and other informations.',
+      target: () => ref2.current,
+    },
+    {
+      title: 'Global Chat',
+      description: 'Here you can with all quark members.',
+      target: () => ref3.current,
+    },
+    {
+      title: 'Chats',
+      description: 'Here you can open chats to added people.',
+      target: () => ref4.current,
+    },
+  ];
   useEffect(() => {
     const fetchLanguagePack = async () => {
       try {
@@ -28,23 +59,30 @@ const UserMenu: React.FC = () => {
     <div>
       <Menu theme="dark" mode="inline" >
         <Menu.Item key="-1" icon={<UserOutlined />}>
-          <NavLink to="/home/">{languagePack.people}</NavLink>
+          <NavLink ref={ref1} to="/home/">{languagePack.people}</NavLink>
           <Badge count={favourites.length} style={{ marginLeft: '8px' }} />
         </Menu.Item>
         <Menu.Item key="0" icon={<ScheduleOutlined />}>
-          <NavLink to="/home/Announcements">{languagePack.announcements}</NavLink>
+          <NavLink ref={ref2} to="/home/Announcements">{languagePack.announcements}</NavLink>
         </Menu.Item>
         <Menu.Item key="1" icon={<GlobalOutlined />}>
-          <NavLink to="/home/chat/global">{languagePack.global}</NavLink>
+          <NavLink ref={ref3} to="/home/chat/global">{languagePack.global}</NavLink>
         </Menu.Item>
+
         <span style={{ color: 'gray', marginLeft: '16px', fontSize: 10 }}>{languagePack.channels}</span>
         {favourites ? favourites.map((user: User) => (
           <Menu.Item key={user.id}>
-            <Avatar src={user.pictureUrl} />
-            <NavLink to={`/home/chat/${user.id}`} style={{ marginLeft: "15px" }}>{user.firstName} {user.lastName}</NavLink>
+            <div ref={ref4}>
+              <Avatar src={user.pictureUrl} />
+              <NavLink to={`/home/chat/${user.id}`} style={{ marginLeft: "15px" }}>{user.firstName} {user.lastName}</NavLink>
+            </div>
           </Menu.Item>
         )) : "Loading.."}
+
+
       </Menu>
+      <Tour open={open} onClose={() => dispatch(setOpenModalUserMenu())} steps={steps} />
+
     </div>
   )
 }
